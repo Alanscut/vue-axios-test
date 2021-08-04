b0b93c8e24e07b866419b59b3980ae22ff44d19f
Alanscut <wp_scut@163.com>
2021-08-04 20:08:36 +0800

    add axios and vue-axios

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="reset">reset msg</button>
+    <button @click="getMsg1">get msg by this.axios</button>
+    <button @click="getMsg2">get msg by this.$http</button>
+    <button @click="getMsg3">get msg by Vue.axios</button>
+    <button @click="getMsg4">get msg by Vue.$http</button>
+    <div v-html="msg"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      msg: "Hello, World!",
+    };
+  },
+  methods: {
+    reset() {
+      this.msg = "Hello, World!";
+    },
+    getMsg1() {
+      this.axios
+        .get("http://localhost:3000/greet")
+        .then((res) => (this.msg = res.data));
+    },
+    getMsg2() {
+      this.$http
+        .get("http://localhost:3000/greet")
+        .then((res) => (this.msg = res.data));
+    },
+    getMsg3() {
+      Vue.axios
+        .get("http://localhost:3000/greet")
+        .then((res) => (this.msg = res.data));
+    },
+    getMsg4() {
+      Vue.$http
+        .get("http://localhost:3000/greet")
+        .then((res) => (this.msg = res.data));
+    },
+  },
+};
 </script>
 
 <style>
